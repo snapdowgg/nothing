8405d5ae6aa312122807b22314e3b37c1a8eb594
snapdowgg <116895994+snapdowgg@users.noreply.github.com>
2026-03-03 20:05:44 +0700

    Create Mahasiswa.java

---
+++ practiceArray/Mahasiswa.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class Mahasiswa {
+    String nama;
+    int umur;
+    String jurusan;
+
+    public Mahasiswa(String nama, int umur, String jurusan) {
+        this.nama = nama;
+        this.umur = umur;
+        this.jurusan = jurusan;
+    }
+
+    public void tampilkanDatane() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
