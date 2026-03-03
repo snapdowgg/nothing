a3ec011d3958ae60fd0503a2aa858a60d7924275
snapdowgg <116895994+snapdowgg@users.noreply.github.com>
2026-03-03 20:04:49 +0700

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,73 @@
+
+// Nomor 1 dan nomor 2
+// import java.util.Scanner;
+
+// public class main {
+//     public static void main(String[] args) {
+//         Scanner a = new Scanner(System.in);
+
+//         System.out.println("Masukan nama anda: ");
+//         String nama = a.nextLine();
+
+//         System.out.println("Masukan umurmu: ");
+//         int umur = a.nextInt();
+
+//         System.out.println("Halo, nama saya " + nama + " dan umur saya " + umur + " tahun.");
+//     }
+// }
+
+// Nomor 3
+
+// import java.util.Scanner;
+
+// public class main {
+//     public static void main(String[] args) {
+//         Scanner b = new Scanner(System.in);
+
+//         System.out.println("Masukan nilai: ");
+//         int nilai = b.nextInt();
+
+//         if(nilai >= 75) {
+//             System.out.println("Selamat, kamu lulus!");
+//         } else {
+//             System.out.println("Maaf, kamu ga lulus!");
+//         }
+//     }
+// }
+
+// Nomor 4
+// Kelipatan Lima
+
+// public class main {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 20; i++) {
+//             if (i % 5 == 0) {
+//                 System.out.println(i + " Kelipatan lima");
+//             } else {
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+// }
+
+// Nomor 5
+
+// import java.util.Random;
+// import java.util.Scanner;
+
+// public class main {
+//     public static void main(String[] args) {
+//         Random angka = new Random();
+//         Scanner a = new Scanner(System.in);
+
+//         int angkarandom = angka.nextInt(10);
+//         System.out.println("Tebak angka yang benar: ");
+//         int tebak = a.nextInt();
+
+//         if(tebak == angkarandom) {
+//             System.out.println("Selamat, tebakanmu benar!");
+//         } else {
+//             System.out.println("Maaf, tebakanmu salah!");
+//         }
+//     }
+// }
